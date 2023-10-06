235fcb7007908ce6c4837848004ff6c5ffbebe22
rimi <a.fatimaarimi@gmail.com>
2023-10-06 15:45:15 -0400

    added react-router-dom

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,7 +1,18 @@
+import { Routes, Route } from "react-router-dom";
+import AuthPage from "./pages/user_authorization/AuthPage";
+import SignIn from "./pages/user_authorization/SignIn";
+import SignUp from "./pages/user_authorization/SignUp";
+import HomePage from "./pages/user_home/HomePage";
+
 export default function App() {
   return (
     <div>
-      <h1>App</h1>
+      <Routes>
+        <Route path="/" element={<AuthPage />} />
+        <Route path="/SignIn" element={<SignIn />} />
+        <Route path="/SignUp" element={<SignUp />} />
+        <Route path="/Home" element={<HomePage />} />
+      </Routes>
     </div>
   );
 }

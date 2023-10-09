00f13b5e4056da651afb234e712cb7365a565ffa
rimi <a.fatimaarimi@gmail.com>
2023-10-09 18:19:56 -0400

    installed dependency (path) && connected frontend/backend

--- server/server.js
+++ server/server.js
@@ -1,17 +1,32 @@
 const express = require("express");
+const path = require("path");
+const dotenv = require("dotenv");
 const connectDB = require("./config/connectDB"); // ? I initially used an import statement just to keep a similar flow from front-end but it was not recognizing the path -- i had to remove type module from my package.json and use req instead
 // Todo: Look into why this didn't work ^^
 
 // dotenv: Load environment variables
-const dotenv = require("dotenv");
 dotenv.config();
 
 // Create an Express app
 const app = express();
 const port = process.env.PORT || 3001;
 
-app.get("/", (req, res) => {
-  res.send("And we are live!");
+// Middleware
+app.use(express.json()); // Parse JSON data
+app.use(express.urlencoded({ extended: true })); // extended: true -> because I will be using multiple schemas (embedded schemas)
+
+// Serve static files from the "dist" directory
+const distPath = path.join(__dirname, "../client/dist");
+app.use(express.static(distPath));
+
+/**-----------------|
+ **Catch All Route* |
+ * -----------------|
+ *
+ * For all other requests that do not have a distinct route, serve the 'index.html' file
+ */
+app.get("/*", (req, res) => {
+  res.sendFile(path.join(distPath, "index.html"));
 });
 
 (async () => {

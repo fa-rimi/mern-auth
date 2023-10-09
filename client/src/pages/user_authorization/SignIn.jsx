74bbeab891cae28919a4b47fd9c468378fd30240
rimi <a.fatimaarimi@gmail.com>
2023-10-09 17:49:39 -0400

    created sign in/sign up form

--- client/src/pages/user_authorization/SignIn.jsx
+++ client/src/pages/user_authorization/SignIn.jsx
@@ -1,13 +1,66 @@
+import { useState } from "react";
 import { Link } from "react-router-dom";
+import { PiEye, PiEyeClosed } from "react-icons/pi";
 
 export default function SignIn() {
+  // Initialize variable
+  const [userLogin, setUserLogin] = useState({
+    name: "",
+    password: "",
+    error: "",
+  });
+
+  const [showPassword, setShowPassword] = useState(false);
+
+  const handleChange = (e) => {
+    setUserLogin({
+      // Spread
+      ...userLogin,
+      // Replace
+      [e.target.name]: e.target.value,
+      error: "",
+    });
+  };
+
+  const handlePasswordToggle = () => {
+    setShowPassword(!showPassword);
+  };
 
   return (
     <div>
       <div>
         <form action="" className="flex flex-col gap-4">
-          <input type="text" name="name" />
-          <input type="password" name="password" />
+          {/* input field for user's name */}
+          <input
+            type="text"
+            name="name"
+            placeholder="Enter Name"
+            value={userLogin.name}
+            onChange={handleChange}
+            autoComplete="off"
+            required
+          />
+
+          {/* User's password with hide password toggle */}
+          <div className="password-input flex items-center relative">
+            {/* input field for user's password */}
+            <input
+              type={showPassword ? "text" : "password"} // If showPassword is true display visible password (text) otherwise hide password (password)
+              name="password"
+              placeholder="Enter Password"
+              value={userLogin.password}
+              onChange={handleChange}
+              className="w-full"
+              required
+            />
+            {/* Show/Hide password toggle */}
+            <span
+              onClick={handlePasswordToggle}
+              className="absolute right-2 cursor-pointer">
+              {/* If showPassword is true render <PiEyeClosed/>: else if false render <PiEye/> */}
+              {showPassword ? <PiEyeClosed /> : <PiEye />}
+            </span>
+          </div>
           <button type="submit">Login</button>
         </form>
       </div>
--- client/src/pages/user_authorization/SignUp.jsx
+++ client/src/pages/user_authorization/SignUp.jsx
@@ -1,20 +1,67 @@
+import { useState } from "react";
 import { Link } from "react-router-dom";
 
 export default function SignUp() {
+  const [registerUser, setRegisterUser] = useState({
+    name: "",
+    email: "",
+    password: "",
+    confirm: "",
+    error: "",
+  });
+
+  const handleChange = (e) => {
+    setRegisterUser({
+      ...registerUser,
+      [e.target.name]: e.target.value,
+      error: "",
+    });
+  };
+
   return (
     <div>
       <form action="" className="flex flex-col gap-4">
         {/* input field for user's name */}
-        <input type="text" name="name" placeholder="Enter Name"/>
+        <input
+          type="text"
+          name="name"
+          placeholder="Enter Name"
+          value={registerUser.name}
+          onChange={handleChange}
+          autoComplete="off"
+          required
+        />
 
         {/* input field for user's email */}
-        <input type="email" name="email" placeholder="Enter Email" />
+        <input
+          type="email"
+          name="email"
+          placeholder="Enter Email"
+          value={registerUser.email}
+          onChange={handleChange}
+          autoComplete="off"
+          required
+        />
 
         {/* input field for user's email */}
-        <input type="password" name="password" placeholder="Enter Password"/>
+        <input
+          type="password"
+          name="password"
+          placeholder="Enter Password"
+          value={registerUser.password}
+          onChange={handleChange}
+          required
+        />
 
         {/* input field for user's confirmed password */}
-        <input type="password" name="confirm" placeholder="Confirm Password"/>
+        <input
+          type="password"
+          name="confirm"
+          placeholder="Confirm Password"
+          value={registerUser.confirm}
+          onChange={handleChange}
+          required
+        />
 
         {/* Submit Button */}
         <button type="submit">Register Now</button>

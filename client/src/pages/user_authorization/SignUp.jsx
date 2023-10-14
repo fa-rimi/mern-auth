055f2e80c66d36bdef3930c09b52b42dc42ec6a5
rimi <a.fatimaarimi@gmail.com>
2023-10-13 22:06:44 -0400

    completed sign up form

--- client/src/pages/user_authorization/SignUp.jsx
+++ client/src/pages/user_authorization/SignUp.jsx
@@ -1,9 +1,15 @@
 import { useState } from "react";
-import { Link } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
+import { toast } from "react-hot-toast";
+import { FaCheckCircle } from "react-icons/fa";
+import axios from "axios";
 
 export default function SignUp() {
+  const navigate = useNavigate();
+
   const [registerUser, setRegisterUser] = useState({
-    name: "",
+    firstName: "",
+    lastName: "",
     email: "",
     password: "",
     confirm: "",
@@ -12,26 +18,76 @@ export default function SignUp() {
 
   const handleChange = (e) => {
     setRegisterUser({
+      // Spread data
       ...registerUser,
+      // Replace with user input
       [e.target.name]: e.target.value,
       error: "",
     });
   };
 
+  const isPasswordValid =
+    registerUser.confirm === registerUser.password &&
+    registerUser.confirm.length >= 6;
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+
+    const { firstName, lastName, email, password } = registerUser;
+
+    try {
+      const response = await axios.post("/SignUp", {
+        firstName,
+        lastName,
+        email,
+        password,
+      });
+
+      if (response.error) {
+        toast.error(response.error);
+      } else {
+        setRegisterUser({
+          firstName: "",
+          lastName: "",
+          email: "",
+          password: "",
+        });
+        toast.success("Registered Successfully!");
+        navigate("/SignIn");
+      }
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
   return (
     <div>
-      <form action="" className="flex flex-col gap-4">
-        {/* input field for user's name */}
-        <input
-          type="text"
-          name="name"
-          placeholder="Enter Name"
-          value={registerUser.name}
-          onChange={handleChange}
-          autoComplete="off"
-          required
-        />
+      <form onSubmit={handleSubmit} className="flex flex-col gap-4">
+        <span className="flex flex-row gap-3 justify-center">
+          {/* input field for user's first name */}
+          <input
+            type="text"
+            name="firstName"
+            placeholder="Enter First Name"
+            value={registerUser.firstName}
+            onChange={handleChange}
+            autoComplete="off"
+            className="p-2"
+            required
+          />
 
+          {/* input field for user's last name */}
+          <input
+            type="text"
+            name="lastName"
+            placeholder="Enter Last Name"
+            value={registerUser.lastName}
+            onChange={handleChange}
+            autoComplete="off"
+            className="p-2"
+            required
+          />
+        </span>
         {/* input field for user's email */}
         <input
           type="email"
@@ -40,6 +96,7 @@ export default function SignUp() {
           value={registerUser.email}
           onChange={handleChange}
           autoComplete="off"
+          className="p-2"
           required
         />
 
@@ -50,21 +107,32 @@ export default function SignUp() {
           placeholder="Enter Password"
           value={registerUser.password}
           onChange={handleChange}
+          className="p-2"
           required
         />
+        <div className="relative flex items-center">
+          {/* input field for user's confirmed password */}
+          <input
+            type="password"
+            name="confirm"
+            placeholder="Confirm Password"
+            value={registerUser.confirm}
+            onChange={handleChange}
+            className="p-2 w-full"
+            required
+          />
 
-        {/* input field for user's confirmed password */}
-        <input
-          type="password"
-          name="confirm"
-          placeholder="Confirm Password"
-          value={registerUser.confirm}
-          onChange={handleChange}
-          required
-        />
-
+          {isPasswordValid && (
+            <FaCheckCircle
+              className="checkmark-icon absolute right-3"
+              color="green"
+            />
+          )}
+        </div>
         {/* Submit Button */}
-        <button type="submit">Register Now</button>
+        <button type="submit" disabled={!isPasswordValid}>
+          Create Account
+        </button>
       </form>
       <p>
         Already have an account? <Link to="/SignIn">Sign In Here</Link>
